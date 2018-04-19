90256f13839505c522bfd6453eae0ee9afac0613
Ashwin <ashwin@micropyramid.com>
2018-04-19 23:42:28 +0530

    first commit

---
+++ api.js
@@ -0,0 +1,100 @@
+var MongoClient = require('mongodb').MongoClient;
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var db_url = "mongodb://localhost:27017/currency";
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    pathname = url.parse(req.url).pathname;
+    if (pathname.match(/^\/style.css\/$/) || pathname.match(/^\/style.css$/)) {
+        res.writeHead(200, {'Content-type' : 'text/css'});
+        var fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
+        res.write(fileContents);
+        res.end();
+    }
+    else if (req.url === '/home.js') {
+        res.writeHead(200, {'Content-type' : 'text/javascript'});
+        var fileContents = fs.readFileSync('./home.js', {encoding: 'utf8'});
+        res.write(fileContents);
+        res.end();
+    }
+    else if (req.url === '/'){
+        fs.readFile('./home.html', null, function(err, data){
+            if(err){
+                res.writeHead(404);
+                res.write('Contents you are looking are Not Found');
+                res.end();
+            }else{
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+    else if (pathname.match(/^\/api\/latest\/$/) || pathname.match(/^\/api\/latest$/)){
+        MongoClient.connect(db_url, function(err, dbo) {
+        var db = dbo.db("currency");
+        var collection = db.collection('prices');
+        fields = {base:1, date:1, _id:0, rates: 1}
+        query = {'base': "EUR"}
+
+        if(q.query['base']){
+            query = {'base': q.query['base'].toUpperCase()}
+        }
+        if(q.query['symbols']){
+            fields = {base:1, date:1, _id:0}
+            symbols = q.query['symbols'].split(',')
+            for (each in symbols){
+                symbol = 'rates.' + symbols[each].toUpperCase()
+                fields[symbol] = 1;
+            }
+        }
+        var cursor = collection.find(query, {fields: fields}).sort({"date": -1}).limit(1).toArray(function(err, result) {
+            if (err) throw err;
+            dbo.close();
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            if(result[0]){
+                res.write(JSON.stringify(result[0]));
+            }else{
+                res.write(JSON.stringify({}))
+            }
+            res.end();
+            });
+        });
+
+    }else if (pathname.match(/^\/api\/\d{4}([./-])\d{2}\1\d{2}$/)){
+        MongoClient.connect(db_url, function(err, dbo) {
+          var db = dbo.db("currency");
+          var collection = db.collection('prices');
+            fields = {base:1, date:1, _id:0, rates: 1}
+            query = {'base': "EUR", 'date': req.url.split('/api/')[1].split('?')[0].toString()}
+            if(q.query['base']){
+                query['base']= q.query['base'].toUpperCase()
+            }
+            if(q.query['symbols']){
+                fields = {base:1, date:1, _id:0}
+                symbols = q.query['symbols'].split(',')
+                for (each in symbols){
+                    symbol = 'rates.' + symbols[each].toUpperCase()
+                    fields[symbol] = 1;
+                }
+            }
+          var cursor = collection.find(query, {fields: fields}).toArray(function(err, result) {
+            if (err) throw err;
+            dbo.close();
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            if(result[0]){
+                res.write(JSON.stringify(result[0]));
+            }else{
+                res.write(JSON.stringify({}))
+            }
+            res.end();
+          });
+        });
+    }else{
+        res.writeHead(404);
+        res.write('Contents you are looking are Not Found');
+        res.end();
+  }
+}).listen(8080);
---
+++ fetcher.py
@@ -0,0 +1,78 @@
+import urllib2
+from xml.dom.minidom import parse
+import json
+from collections import OrderedDict
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['currency']
+db_prices = db['prices']
+
+u1 = urllib2.urlopen('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
+dom = parse(u1)
+
+itemlist = dom.getElementsByTagName('Cube')
+cur = []
+rates = []
+for s in itemlist:
+    try:
+        cur.append(str(s.attributes['currency'].value))
+        rates.append(float(s.attributes['rate'].value))
+    except:
+        pass
+
+for s in itemlist:
+    try:
+        date = (str(s.attributes['time'].value))
+    except:
+        pass
+
+
+def my_price(source, target):
+    my = float(target) / float(source)
+    return float("{0:.4f}".format(my))
+
+
+def dict_gen(row, currency, currency_codes, date):
+    d = OrderedDict()
+
+    d['base'] = currency
+    d['date'] = date
+    rates = OrderedDict()
+    i = 0
+
+    if currency == "EUR":
+        for code in currency_codes:
+            rates[code] = float(row[i])
+            i += 1
+
+    else:
+        currency_index = currency_codes.index(currency)
+        rates['EUR'] = my_price(row[currency_index], 1)
+        for code in currency_codes:
+
+            if code == currency:
+                i += 1
+                continue
+            rates[code] = my_price(row[currency_index], row[i])
+            i += 1
+
+    rates = OrderedDict(sorted(rates.iteritems()))
+    d['rates'] = rates
+    return d
+
+
+new_line, new_codes = rates[:], cur[:]
+
+#  for EUR
+eur_new_line, eur_new_codes = rates[:], cur[:]
+
+eur_ = dict_gen(eur_new_line, currency="EUR", currency_codes=eur_new_codes, date=date)
+print (json.dumps(eur_))
+db_prices.insert_one(eur_)
+#  end EUR block
+
+for code in new_codes:
+    temp_row = new_line[:]
+    record = dict_gen(temp_row, currency=code, currency_codes=new_codes, date=date)
+    db_prices.insert_one(record)
